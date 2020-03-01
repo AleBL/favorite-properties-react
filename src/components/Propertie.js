82c1b6e8d3eada353bfaaae4f82e7dac0ce83fd6
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-01 16:48:42 -0300

    propertie.css created

--- src/components/Propertie.js
+++ src/components/Propertie.js
@@ -1,20 +1,24 @@
 import React from "react"
 import util from "../util/util"
+import "../css/propertie.css"
 
 const Propertie = (props) => {
+  const classBtnFavorite = props.propertie.favorite ? "flash-container btn btn-favorite" : "none";
+
   return (
-    <div id={ props.propertie.id }>
+    <div id={ props.propertie.id } className="buildings">
 
       <b>{ props.propertie.name }</b>
       <b>{ util.formatCurrencyBRL(props.propertie.min_price) }</b>
-      <b>{ props.propertie.name }</b>
       <b>{ props.propertie.address.city + props.propertie.address.state }</b>
 
       <a>
         <img src={`${ props.propertie.default_image["200x140"] }`} alt={ props.propertie.name } />
       </a>
 
-      <button className="btn-small btn-primary" onClick={ (e)=> props.addToFavorites(props.propertie) }> + </button>
+      <button className={ classBtnFavorite } onClick={ (e)=> props.addToFavorites(props.propertie) }> 
+        &hearts;
+      </button>
     </div>
   )
 }
