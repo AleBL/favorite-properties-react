1d5d2b8abb25f48070abe19a54a7604735586530
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-01 16:47:10 -0300

    highlight current page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,20 +1,20 @@
-import React from "react"
+import React, { Fragment } from "react"
 import "../css/pagination.css"
 
 const Page = (props) => {
   const pageNumber = props.pageNumber + 1;
-  const classes = props.pageNumber === props.currentPage ? 'button active' : 'button';
+  const classBtnAddFavorite = pageNumber === props.currentPage ? 'button active' : 'button';
 
   if (pageNumber == 1 || pageNumber == props.totalPages 
     || (pageNumber >= props.currentPage - 2 && pageNumber <= props.currentPage + 2)
     || (pageNumber >= props.totalPages - 2 && pageNumber <= props.totalPages + 2)) {
 
     return (
-      <button className={ classes } onClick={ ()=> props.loadPage( pageNumber ) }> { pageNumber } </button>
+      <button className={ classBtnAddFavorite } onClick={ ()=> props.loadPage( pageNumber ) }> { pageNumber } </button>
     );
   }
 
-  return ( <div> </div> );
+  return ( <Fragment/> );
 }
 
 export default Page;
