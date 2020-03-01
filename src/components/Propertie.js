9d4064d0a031504ee3389a10ecc0a3f027ba72cc
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-01 19:17:27 -0300

    minor fixes in property.css and Propertie.js

--- src/components/Propertie.js
+++ src/components/Propertie.js
@@ -3,10 +3,10 @@ import util from "../util/util"
 import "../css/propertie.css"
 
 const Propertie = (props) => {
-  const classBtnFavorite = props.propertie.favorite ? "flash-container btn btn-favorite" : "none";
+  const classBtnFavorite = props.propertie.favorite ? "flash-container btn btn-favorite" : "btn btn-primary";
 
   return (
-    <div id={ props.propertie.id } className="buildings">
+    <div id={ props.propertie.id } className="properties">
 
       <b>{ props.propertie.name }</b>
       <b>{ util.formatCurrencyBRL(props.propertie.min_price) }</b>
