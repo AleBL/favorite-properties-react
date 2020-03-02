1588ab9898e0ec0e75aae5e0e0fb3b111782f99c
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-02 13:17:16 -0300

    interval function in util.js created

--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,13 @@
 export default {
   formatCurrencyBRL: function (num) {
       return num.toLocaleString("pt-BR", { style:"currency", currency:"BRL" });
+  },
+
+  interval: function (num1, num2, unit = "") {
+    if (num1 == num2){
+      return num1 + unit;
+    }
+
+    return "Between " + (num1 + unit) + " and " + (num2 + unit);
   }
 }
