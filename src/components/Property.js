9925c9c5d7b6bdf460ebb777a4af2005567bd0d8
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-02 13:21:21 -0300

    new component PropertyModal  being rendered inside Property

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropertyModal from "./PropertyModal.js"
 import util from "../util/util"
 import "../css/property.css"
 
@@ -7,7 +8,6 @@ const Property = (props) => {
 
   return (
     <div id={ props.property.id } className="properties">
-
       <b>{ props.property.name }</b>
       <b>{ util.formatCurrencyBRL(props.property.min_price) }</b>
       <b>{ props.property.address.city + props.property.address.state }</b>
@@ -16,9 +16,14 @@ const Property = (props) => {
         <img src={`${ props.property.default_image["200x140"] }`} alt={ props.property.name } />
       </a>
 
-      <button className={ classBtnFavorite } onClick={ (e)=> props.addToFavorites(props.property) }> 
-        &hearts;
-      </button>
+      <div className="containerButtons">
+        <button className={ classBtnFavorite } onClick={ (e)=> props.addToFavorites(props.property) }> 
+          &hearts;
+        </button>
+
+        <PropertyModal
+          property={ props.property } />
+      </div>
     </div>
   )
 }
