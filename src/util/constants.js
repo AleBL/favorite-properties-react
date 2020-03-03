074d5b26e115b93c8fc7310e94d0bf840675c6a4
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-03-03 16:04:30 -0300

    constants.js created

---
+++ src/util/constants.js
@@ -0,0 +1,5 @@
+export const baseApiPath = "https://www.orulo.com.br/api/v2/buildings/";
+export const bearerToken = "<ADD BEARER TOKEN HERE>";
+
+export const googleMapsKey = "<ADD GOOGLE API KEY HERE>";
+export const zoomDefault = 17;
